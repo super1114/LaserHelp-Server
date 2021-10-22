fbc2b853f90f311af54d89f0f2a6cfaa3350fd7e
super1114 <super1114dev@gmail.com>
2021-10-22 10:49:04 -0400

    final

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const fileUpload = require('express-fileupload');
 const morgan = require('morgan');
 const _ = require('lodash');
-// const Question = require("./server/models/questions.model");
+const Question = require("./server/models/questions.model");
 
 const app = express();
 
@@ -18,43 +18,17 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(morgan('dev'));
 
-// const formData = require('express-form-data');
-// app.use(formData.parse());
 app.post('/api/submit_question', async (req, res) => {
-  // console.log(req.files)
-  // Question.Request(req.body, req.files, function(err, result) {
-  //   if(result.length==0) return res.json({status:false});
-  //   else return res.json({status:true, result:result});
-  // });
-
-  try {
-      if(!req.files) {
-          res.send({
-              status: false,
-              message: 'No file uploaded'
-          });
-      } else {
-        
-          //Use the name of the input field (i.e. "avatar") to retrieve the uploaded file
-          let avatar = req.files.file;
-          
-          //Use the mv() method to place the file in upload directory (i.e. "uploads")
-          avatar.mv('./server/uploads/' + avatar.name);
-          // console.log(avatar);
-          //send response
-          res.send({
-              status: true,
-              message: 'File is uploaded',
-              data: {
-                  name: avatar.name,
-                  mimetype: avatar.mimetype,
-                  size: avatar.size
-              }
-          });        
-      }
-  } catch (err) {
-      res.status(500).send(err);
-  }  
+  let filename = "";
+  if(req.files) {
+    let myfile = req.files.file;
+    filename = Date.now()+myfile.name.substring(myfile.name.indexOf("."), myfile.name.length);
+    myfile.mv('./server/uploads/' + filename);
+  }
+  Question.submitQuestion(req.body, filename, function(err, result) {
+    if(err) return res.send({status:false, message: "Failed"});
+    else res.send({ status:true, message: "Success", questionId:result.insertId });
+  });
 });
 
 
--- server/routes/customer.routes.js
+++ server/routes/customer.routes.js
@@ -4,4 +4,7 @@ module.exports = app => {
   app.post("/api/signup", laserhelp.signUp);
   app.post("/api/login", laserhelp.logIn);
   app.post("/api/submit_question", laserhelp.Request);
+  app.get("/api/fetch_response", laserhelp.fetchResponse);
+  app.get("/api/download/:id", laserhelp.downloadFile);
+  app.post("/api/submitResponse", laserhelp.submitResponse);
 };
